62246a637a7a42d44451d7a12f52814115bf937b
Anisha Swain <anisha.swain@yahoo.in>
2018-01-11 16:33:33 +0530

    ejs theme setup

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var index = require('./routes/index');
 var result = require('./routes/result');
@@ -26,23 +27,27 @@ app.use('/result',result);
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views')
 
+//set static files(css or js or imgs)
+app.use(express.static(__dirname + "/public"));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 module.exports = app;
+
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var path = require("path");
+module.exports = router;
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mark-updater' });
+router.get('/',function(req,res){
+	console.log("Got get request");
+	res.sendFile(path.join(__dirname,'../index.html'));
 });
 
-module.exports = router;
