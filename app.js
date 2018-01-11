c86aca2a82c87a1ffcfb9e6c104d77e9aed8d43f
Anisha Swain <anisha.swain@yahoo.in>
2018-01-11 15:28:11 +0530

    express ejs setup

--- app.js
+++ app.js
@@ -6,13 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var users = require('./routes/users');
+var result = require('./routes/result');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,8 +18,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+//setup route
+app.use('/',index);
+app.use('/result',result);
+
+//setup ejs
+app.set('view engine','ejs');
+app.set('views',__dirname+'/views')
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
