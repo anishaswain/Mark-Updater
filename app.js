17d2fd21b3c0ac2557e55a9130cc70f678aa999d
Anisha Swain <anisha.swain@yahoo.in>
2018-01-12 12:59:12 +0530

    database connected

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require("path");
+var mysql = require("mysql");
 
 var index = require('./routes/index');
 var result = require('./routes/result');
@@ -53,4 +54,45 @@ app.use(express.static(__dirname + "/public"));
 //   res.render('error');
 // });
 
+
+//set connection to database
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "chipkili123",
+  database: "testdb"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  // var sql = "CREATE TABLE test (REGD_NO INT NOT NULL, STREAM VARCHAR(255),BRANCH VARCHAR(255), STUDENT_NO INT NOT NULL,Sub_Code VARCHAR(255), Subject VARCHAR(255), Sem VARCHAR(255))";
+  // con.query(sql, function (err, result) {
+  //   if (err) throw err;
+  //   console.log("Table created");
+  // });
+  con.query("SELECT * FROM test", function (err, result, fields) {
+    if (err) throw err;
+    // console.log(result); 
+  });
+
+});
+
+var obj = {};
+app.get('/result', function(req, res){
+
+    connection.query('SELECT * FROM test', function(err, result) {
+
+        if(err){
+            throw err;
+        } else {
+            obj = {print: result};
+            res.render('index', obj);
+            console.log(obj);                
+        }
+    });
+
+});
+
 module.exports = app;
+
