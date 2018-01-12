b77487eaf2842fb9035ea64669d3059a998e92ba
Anisha Swain <anisha.swain@yahoo.in>
2018-01-12 13:00:40 +0530

    database connected-2

--- routes/result.js
+++ routes/result.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('result');
 });
 
 module.exports = router;
