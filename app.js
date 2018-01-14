786011fa84d22a83e5359cdb3b429c87ff49267a
Rishav Agarwal <rishav.159@gmail.com>
2018-01-14 14:14:59 +0530

    Code restructure

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var path = require("path");
-var mysql = require("mysql");
 
 var index = require('./routes/index');
 var result = require('./routes/result');
@@ -13,7 +12,12 @@ var login = require('./routes/login');
 var signup = require('./routes/signup');
 
 var app = express();
+var database = require('./database.js');
 
+database.connection.connect(function(err) {
+  if (err) throw err;
+  console.log("Databse is now connected!");
+});
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -36,63 +40,22 @@ app.set('views',__dirname+'/views')
 app.use(express.static(__dirname + "/public"));
 
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-
-//set connection to database
-var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "chipkili123",
-  database: "testdb"
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-con.connect(function(err) {
-  if (err) throw err;
-  console.log("Connected!");
-  // var sql = "CREATE TABLE test (REGD_NO INT NOT NULL, STREAM VARCHAR(255),BRANCH VARCHAR(255), STUDENT_NO INT NOT NULL,Sub_Code VARCHAR(255), Subject VARCHAR(255), Sem VARCHAR(255))";
-  // con.query(sql, function (err, result) {
-  //   if (err) throw err;
-  //   console.log("Table created");
-  // });
-  con.query("SELECT * FROM test", function (err, result, fields) {
-    if (err) throw err;
-    // console.log(result); 
-  });
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-});
-
-var obj = {};
-app.get('/result', function(req, res){
-
-    connection.query('SELECT * FROM test', function(err, result) {
-
-        if(err){
-            throw err;
-        } else {
-            obj = {print: result};
-            res.render('index', obj);
-            console.log(obj);                
-        }
-    });
-
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
-
--- routes/result.js
+++ routes/result.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
-
+var database = require('../database.js');
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('result');
+router.get('/:branchid', function(req, res, next) {
+  query = "Select * FROM "+database.tablename+" WHERE BRANCH = '" +req.params.branchid+"';";
+  database.connection.query(query,function(error,results,fields){
+    if(error){
+      console.log("Error found");
+    }else{
+      res.render('result',{results:results});
+    }
+  });
 });
 
 module.exports = router;
