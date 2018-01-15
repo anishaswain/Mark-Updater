1d0e31811bc1bca11b6436aa256249eadfdf14dd
Anisha Swain <anisha.swain@yahoo.in>
2018-01-16 02:45:37 +0530

    branch dropdown working

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,47 @@ $('#sem_select').on('change',function() {
   $.ajax({
     method : 'GET',
     url : '/api/'+sem+'/getbranch',
+    dataType: 'json',
     success : function(data){
-      console.log(data);
+      var $el = $("#branch_select");
+                    $el.empty(); // remove old options
+                    $el.append($("<option></option>")
+                            .attr("value", '').text('Please Select'));
+                    $.each(data, function(value, key) {
+                        $el.append($("<option></option>")
+                                .attr("value", value).text(key.BRANCH));
+                    });                           
+                  
+
+
+
+    }
+  })
+})
+
+$('#subject_select').on('change',function() {
+  // var sem = $('#sem_select').val();
+  // console.log(sem);
+  var subj = $(this).val();
+  console.log(subj);
+  $.ajax({
+    method : 'GET',
+    url : '/api/'+sem+subj+'/getsubj',
+    dataType: 'json',
+    success : function(data){
+      console.log(data[0]);
+      var $el = $("#branch_select");
+                    $el.empty(); // remove old options
+                    $el.append($("<option></option>")
+                            .attr("value", '').text('Please Select'));
+                    $.each(data, function(value, key) {
+                        $el.append($("<option></option>")
+                                .attr("value", value).text(key.BRANCH));
+                    });                           
+                  
+
+
+
     }
   })
 })
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/',function(req,res){
 });
 
 router.get('/api/:sem/getbranch', function(req,res){
-	var query = 'SELECT BRANCH FROM '+database.tablename+' WHERE SEM = "'+req.params.sem+'" GROUP BY BRANCH;';
+	var query = 'SELECT BRANCH FROM '+database.tablename+' WHERE Sem = "'+req.params.sem+'" GROUP BY BRANCH;';
+	console.log(query)
 	database.connection.query(query, function(error,results,fields) {
 		if(error){
 			console.log(error);
@@ -20,4 +21,18 @@ router.get('/api/:sem/getbranch', function(req,res){
 		}
 	});
 });
+
+router.get('/api/:sem/:subj/getsubj', function(req,res){
+	var query = 'SELECT Subject FROM '+database.tablename+' WHERE Sem = "'+req.params.sem+'" GROUP BY Subject;';
+	console.log(query)
+	database.connection.query(query, function(error,results,fields) {
+		if(error){
+			console.log(error);
+		}else{
+			console.log(results);
+			res.send(results);
+		}
+	});
+});
+
 module.exports = router;
