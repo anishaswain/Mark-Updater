a30196acc3f9508f5d2d9e7151e3056152f2c3d3
Manaswini Das <dasmanaswini10@gmail.com>
2018-01-16 14:19:55 +0530

    Login and Signup frontend fixes

--- database.js
+++ database.js
@@ -4,11 +4,11 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "omega160",
-  database: "test"
+  password: "Mumuli@10",
+  database: "testdb"
 });
 
-var tablename= "students";
+var tablename= "test";
 module.exports = {
   tablename : tablename,
   connection : con
