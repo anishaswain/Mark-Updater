6e80f11695f3f4a1c5bf61ef7bdd53afeedf6dc6
Anisha Swain <anisha.swain@yahoo.in>
2018-01-17 21:51:08 +0530

    result page added

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Mumuli@10",
+  password: "chipkili123",
   database: "testdb"
 });
 
